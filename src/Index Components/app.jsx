1802afd1e26f48dc2c4f22c611bfe6ceb8eafed1
zamion <antesoft17@outlook.com>
2019-10-15 17:37:07 +0500

    Made an Index Component directory and replaced css files with scss

---
+++ src/Index Components/app.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './app.scss'
+
+export class app extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                App Work ! 
+            </div>
+        )
+    }
+}
+
+export default app
